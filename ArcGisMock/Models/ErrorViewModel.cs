e32d9b6c0cd150e13711434dbccbf9c5f11b9b92
Pedro Martins <pedro.mourao.martins@example.com>
2024-09-04 10:17:37 +0100

    Add project files.

---
+++ ArcGisMock/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArcGisMock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
